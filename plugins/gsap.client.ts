bfe79dd6db931d9f5ca097633c5000ab0d5420a5
Favour  Emeka <favorodera@gmail.com>
2025-06-22 15:38:31 +0100

    refactor: simplify GSAP plugin initialization and improve error handling

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -1,43 +1,37 @@
-export default defineNuxtPlugin(async (nuxtApp) => {
-  const provideNulls = () => {
-    nuxtApp.provide('gsap', null)
-    nuxtApp.provide('ScrollTrigger', null)
-    nuxtApp.provide('ScrollToPlugin', null)
-    nuxtApp.provide('Draggable', null)
-    nuxtApp.provide('TextPlugin', null)
+export default defineNuxtPlugin(async () => {
+  async function importGSAP() {
+    try {
+      const [{ default: gsap }, { default: ScrollTrigger }, { default: ScrollToPlugin }, { default: Draggable }, { default: TextPlugin }] = await Promise.all([
+        import('gsap'),
+        import('gsap/ScrollTrigger'),
+        import('gsap/ScrollToPlugin'),
+        import('gsap/Draggable'),
+        import('gsap/TextPlugin'),
+      ])
+
+      return {
+        gsap,
+        ScrollTrigger,
+        ScrollToPlugin,
+        Draggable,
+        TextPlugin,
+      }
+    }
+    catch {
+      console.error('GSAP not installed! To enable animations, run: npm install gsap')
+      return null
+    }
   }
 
-  try {
-    const [
-      { gsap },
-      { ScrollTrigger },
-      { ScrollToPlugin },
-      { Draggable },
-      { TextPlugin },
-    ] = await Promise.all([
-      import('gsap'),
-      import('gsap/ScrollTrigger'),
-      import('gsap/ScrollToPlugin'),
-      import('gsap/Draggable'),
-      import('gsap/TextPlugin'),
-    ])
+  const gsapModules = await importGSAP()
 
-    gsap.registerPlugin(
-      ScrollTrigger,
-      ScrollToPlugin,
-      Draggable,
-      TextPlugin,
-    )
-
-    nuxtApp.provide('gsap', gsap)
-    nuxtApp.provide('ScrollTrigger', ScrollTrigger)
-    nuxtApp.provide('ScrollToPlugin', ScrollToPlugin)
-    nuxtApp.provide('Draggable', Draggable)
-    nuxtApp.provide('TextPlugin', TextPlugin)
-
-  }
-  catch {
-    console.warn('GSAP not installed! To enable animations, run: ' + '`npm install gsap`')
-    provideNulls()
+  return {
+    provide: {
+      gsap: gsapModules?.gsap,
+      ScrollTrigger: gsapModules?.ScrollTrigger,
+      ScrollToPlugin: gsapModules?.ScrollToPlugin,
+      Draggable: gsapModules?.Draggable,
+      TextPlugin: gsapModules?.TextPlugin,
+    },
   }
 })
